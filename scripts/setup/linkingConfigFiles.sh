3f87a90782ceab402d8dafe4607c8e184e016e6b
CodingIsLove <christopher.germann.eth@gmail.com>
2019-12-26 14:11:12 +0100

    File linking script is working

--- scripts/setup/linkingConfigFiles.sh
+++ scripts/setup/linkingConfigFiles.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-
 FILE_PATH="./../../dotFiles/.*"
-
 
 setupLinks(){
 	for i in $FILE_PATH; do
 		if [[ -f $i ]]; then
-			echo $i | grep -E ".[a-z]+rc$" > file.txt 
+			var=$(basename $i) 
+
+			# delte file if it already exists 
+			if [[ -e ~/$var ]]; then
+				rm ~/$var	
+			elif [[ -h ~/$var ]]; then
+				unlink ~/$var
+			fi
+
+			# Link the files
+			ln -s $PWD/$var ~/$var
 		fi
 	done
 }
