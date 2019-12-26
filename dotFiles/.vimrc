2c0cb7cfa4f0f72fe72728c3f0d51e6c14034852
CodingIsLove <christopher.germann.eth@gmail.com>
2019-12-26 15:44:18 +0100

    Added Elements for pathogen and rewrote linking config files, so it also will ad directories

--- dotFiles/.vimrc
+++ dotFiles/.vimrc
@@ -1,6 +1,4 @@
-
 " Vim configuration file. Written by Christopher Germann
-"
 
 " Turn on syntax highlighting
 syntax on
@@ -13,3 +11,21 @@ set laststatus=2
 
 " show numbers
 set number
+
+" Dont try to be vi compatible
+set nocompatible
+
+" Show file stats
+set ruler
+
+" Blink cursor on error instead of beeping
+set visualbell
+
+" Searching
+set hlsearch
+set incsearch
+set ignorecase
+set smartcase
+set showmatch
+
+
--- scripts/setup/linkingConfigFiles.sh
+++ scripts/setup/linkingConfigFiles.sh
@@ -6,18 +6,21 @@ HOME_CONFIG_FILES="/Documents/GitRepos/BasicConfigurations/dotFiles"
 
 setupLinks(){
 	for i in $FILE_PATH/.*; do
-		if [[ -f $i ]]; then
+		if [[ -f $i ]] || [[ -d $i ]]; then
 			var=$(basename $i) 
 
-			# delte file if it already exists 
-			if [[ -e ~/$var ]]; then
-				rm ~/$var	
-			elif [[ -h ~/$var ]]; then
-				unlink ~/$var
-			fi
-
+			if [[ ! "$var" =~ \.$ ]]; then
+				# delte file/director/link if it already exists 
+				if [[ -e ~/$var ]]; then
+					rm ~/$var	
+				elif [[ -h ~/$var ]]; then
+					unlink ~/$var
+				elif [[ -d ~/$var ]]; then
+					rm -r ~/$var
+				fi
 			# Link the files
 			ln -s ~/$HOME_CONFIG_FILES/$var ~/$var
+			fi
 		fi
 	done
 }
@@ -25,3 +28,5 @@ setupLinks(){
 
 # Execute the code
 setupLinks
+echo "All config files are linked now"
+
